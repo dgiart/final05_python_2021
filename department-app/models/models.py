abfe4b89b69c57e92ac1baf237e12244266222b3
ULXOcMYh <babichart@gmail.com>
2021-05-28 16:07:51 +0300

    28/05/2021, ORM does not work, try to do cimpler db.

--- department-app/models/models.py
+++ department-app/models/models.py
@@ -4,24 +4,24 @@ from datetime import datetime
 db = setup.db
 
 
-depts_empls=db.Table('depts_empls',
-    db.Column('id_dept', db.Integer, db.ForeignKey('department.id_dept')),
-    db.Column('id_empl', db.Integer, db.ForeignKey('employee.id_empl'))
-
-)
+# depts_empls=db.Table('depts_empls',
+#     db.Column('id_dept', db.Integer, db.ForeignKey('department.id_dept')),
+#     db.Column('id_empl', db.Integer, db.ForeignKey('employee.id_empl'))
+#
+# )
 
 
 class Department(db.Model):
     id_dept = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(140))
-    employees = db.relationship('Employee', secondary=depts_empls,  backref='department', lazy='dynamic')
+    # employees = db.relationship('Employee', secondary=depts_empls,  backref='department', lazy='dynamic')
 
 
     def __init__ (self, *args, **kwargs):
         super(Department, self). __init__(*args, **kwargs)
 
     def __repr__(self):
-        return f'title: {self.title}, id={self.id_dep}, employees={self.employees}'
+        return f'title: {self.title}, id={self.id_dep})'#, employees={self.employees}'
 
 
 class Employee(db.Model):
@@ -29,7 +29,7 @@ class Employee(db.Model):
     name = db.Column(db.String(140))
     salary = db.Column(db.Float)
     birthday = db.Column(db.DateTime)
-    id_dept = db.Column(db.Integer, db.ForeignKey('department.id_dept'))
+    id_dept = db.Column(db.Integer)#, db.ForeignKey('department.id_dept'))
 
     def __init__ (self, *args, **kwargs):
         super(Employee, self). __init__(*args, **kwargs)
