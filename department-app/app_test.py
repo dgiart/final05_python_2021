aef473a161ed3a50eaaafee7fbb302e706dd5bc8
ULXOcMYh <babichart@gmail.com>
2021-05-28 15:24:18 +0300

    28/05/2021, ORM does not work, try to do cimpler db.

--- department-app/app_test.py
+++ department-app/app_test.py
@@ -1,45 +1,60 @@
-from models.models import Departments
+from models.models import Department, Employee
+from datetime import datetime
+from setup import db
 # import random
 from flask import Flask, request, render_template
 from collections import Counter
 
-app = Flask(__name__)
-departments = []
-count = 0
-
-
-def correct_request(req_args, model_args):
-    return Counter(req_args) == Counter(model_args)
-
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    global count
-    flask_request = request
-    if flask_request.method == 'GET':
-        # dept_args = Departments.dept_args
-        # return {'Departments': dept_args}
-        # return {'departments': departments, 'Departments': dept_args}
-        return render_template('index.html', departments=departments)#, n=name1, m=name2, list=l)
-    if flask_request.method == 'POST':
-        req_args = flask_request.args.to_dict()
-        model_args = Departments.dept_args
-        print(f'flask_request: {flask_request.args}')
-        print(f'req_args = {req_args}')
-        print(f'model_args = {model_args}')
-        print(correct_request(req_args.keys(), model_args))
-        if correct_request(req_args.keys(), model_args):
-            count += 1
-            dep_name = req_args['name']
-            dep_num = req_args['num']
-            # department = Departments(dep_name, dep_num)
-            department = {'id': count, 'name': dep_name, 'num': dep_num}
-            departments.append(department)
-            print(departments)
-            return flask_request.args
-        else:
-            return 'Wrong request'
+# app = Flask(__name__)
+# departments = []
+# count = 0
+#
+#
+# def correct_request(req_args, model_args):
+#     return Counter(req_args) == Counter(model_args)
+#
+#
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+#     global count
+#     flask_request = request
+#     if flask_request.method == 'GET':
+#         # dept_args = Departments.dept_args
+#         # return {'Departments': dept_args}
+#         # return {'departments': departments, 'Departments': dept_args}
+#         return render_template('index.html', departments=departments)#, n=name1, m=name2, list=l)
+#     if flask_request.method == 'POST':
+#         req_args = flask_request.args.to_dict()
+#         model_args = Departments.dept_args
+#         print(f'flask_request: {flask_request.args}')
+#         print(f'req_args = {req_args}')
+#         print(f'model_args = {model_args}')
+#         print(correct_request(req_args.keys(), model_args))
+#         if correct_request(req_args.keys(), model_args):
+#             count += 1
+#             dep_name = req_args['name']
+#             dep_num = req_args['num']
+#             # department = Departments(dep_name, dep_num)
+#             department = {'id': count, 'name': dep_name, 'num': dep_num}
+#             departments.append(department)
+#             print(departments)
+#             return flask_request.args
+#         else:
+#             return 'Wrong request'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    e1= Employee(name='TestEmpl')
+    # e1.id_empl = 10000
+    e1.birthday = datetime.now().date()
+    e1.id_dept = 10000
+    d1 = Department(title='NewModelTest')
+    d1.id_dep = 10000
+    d1.employees.append(e1)
+    db.session.add(d1)#, d1)
+    db.session.commit()
+    print(e1)
+    # print(d1.employees)
+
+
--- department-app/models/models.py
+++ department-app/models/models.py
@@ -4,22 +4,36 @@ from datetime import datetime
 db = setup.db
 
 
+depts_empls=db.Table('depts_empls',
+    db.Column('id_dept', db.Integer, db.ForeignKey('department.id_dept')),
+    db.Column('id_empl', db.Integer, db.ForeignKey('employee.id_empl'))
+
+)
+
+
 class Department(db.Model):
     id_dept = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(140))
-    empoyees = db.relationship('Employee', backref='department',
-                    lazy='dynamic')
+    employees = db.relationship('Employee', secondary=depts_empls,  backref='department', lazy='dynamic')
 
-    def __init__(self, title):
-        self.title = title
+
+    def __init__ (self, *args, **kwargs):
+        super(Department, self). __init__(*args, **kwargs)
 
     def __repr__(self):
-        return f'name: {self.title}, id={self.id_dep}'
+        return f'title: {self.title}, id={self.id_dep}, employees={self.employees}'
 
 
 class Employee(db.Model):
     id_empl = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140))
     salary = db.Column(db.Float)
-    birth = db.Column(db.DateTime)
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id_dept'))
+    birthday = db.Column(db.DateTime)
+    id_dept = db.Column(db.Integer, db.ForeignKey('department.id_dept'))
+
+    def __init__ (self, *args, **kwargs):
+        super(Employee, self). __init__(*args, **kwargs)
+
+    def __repr__(self):
+        return f'id: {self.id_empl}, name: {self.name}, birthday: {self.birthday}, department: {self.id_dept}'
+
